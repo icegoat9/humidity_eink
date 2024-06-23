fc0b979483a579569a6201f154ec8b9cf3b1811e
icegoat9 <85364179+icegoat9@users.noreply.github.com>
2024-06-23 15:43:05 -0700

    test drawing lines of text on display, time (not relevant without RTC)

--- eink_test.py
+++ eink_test.py
@@ -9,7 +9,13 @@ import adafruit_il0373
 import terminalio
 from adafruit_display_text import label
 
+def curtime_as_string():
+  t = time.localtime()
+  tstr = f"{t.tm_year}-{t.tm_mon:02}-{t.tm_mday:02} {t.tm_hour:02}:{t.tm_min:02}"
+  return tstr
+
 print('running eink_test.py...')
+print(curtime_as_string())
 
 # Used to ensure the display is free in CircuitPython
 displayio.release_displays()
@@ -41,16 +47,30 @@ display = adafruit_il0373.IL0373(
 display_group = displayio.Group()
 
 # Create a text object with some content
-text = displayio.Group(scale = 2, x=0, y=0)
+text = displayio.Group(scale = 3, x=10, y=30)
 text_area = label.Label(terminalio.FONT, text="Hello, sailor.")
 text.append(text_area)
 display_group.append(text)
+
+text2 = displayio.Group(scale = 2, x=10, y=70)
+text2_area = label.Label(terminalio.FONT, text="text line 2")
+text2.append(text2_area)
+display_group.append(text2)
+
+text3 = displayio.Group(scale = 2, x=10, y=100)
+text3_area = label.Label(terminalio.FONT, text=curtime_as_string())
+text3.append(text3_area)
+display_group.append(text3)
 
 # Place the display group on the screen
 display.root_group = display_group
 
 while True:
+    #print(f'display refresh at {curtime_as_string()}')
     display.refresh()
     time.sleep(180)   # do not refresh this e ink display faster than 180 seconds
-    # update value
+    # update value of existing display object
+    text2_area.text = "Time passes..."
+    text3_area.text = curtime_as_string()
+    
 
