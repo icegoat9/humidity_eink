d90a6e4ff6f6e1cf7f73896424236bf4c4fb9c05
icegoat9 <85364179+icegoat9@users.noreply.github.com>
2024-12-16 16:52:30 -0800

    remove unused code, run python formatter

--- main_humidity_eink.py
+++ main_humidity_eink.py
@@ -14,6 +14,7 @@ import random
 from adafruit_display_text import label
 from adafruit_display_shapes.line import Line
 from adafruit_display_shapes.circle import Circle
+
 # import adafruit_ahtx0   # previously, lower-accuracy humidity sensor
 import adafruit_sht4x
 import alarm
@@ -39,11 +40,11 @@ rh_sensor = adafruit_sht4x.SHT4x(board.I2C())
 
 # initialize a fixed-length RH buffer and related variables
 # note: these variables may be overwritten below from Sleep Memory if we are resuming after a deep sleep
-rh_data = [0] * (12 * 19)   # 19 days worth of data
+rh_data = [0] * (12 * 19)  # 19 days worth of data
 rh_data_index = 0
 run_cycles = 0
 SLEEP_MINUTES = 120
-data_per_box = 24 * 60 // SLEEP_MINUTES   # how many data points per box (often meaning, per day)
+data_per_box = 24 * 60 // SLEEP_MINUTES  # how many data points per box (often meaning, per day)
 if DEBUG_MODE:
     SLEEP_MINUTES = 3  # Warning: do not update the tricolor E Ink screen more often than every three minutes
 
@@ -80,7 +81,7 @@ if not alarm.wake_alarm:
         DUMMY_BOXES = 4
         for b in range(DUMMY_BOXES):
             for i in range(data_per_box):
-                rh_data[b * data_per_box + i] = 50 - 10 * b + random.randint(-10,10)
+                rh_data[b * data_per_box + i] = 50 - 10 * b + random.randint(-10, 10)
         rh_data_index = DUMMY_BOXES * data_per_box - 1
 else:
     print("waking after deep sleep, loading variables from sleep memory")
@@ -155,7 +156,9 @@ yaxis_labels.append(label.Label(font=FONT, text="RH", color=BLACK))
 yaxis_labels.append(label.Label(x=3, y=11, font=FONT, text="%", color=BLACK))
 graph.append(yaxis_labels)
 
-xaxis_label = label.Label(x=graph_x0 + graph_width // 2 - 12, y=display.height - 10, font=FONT, text='days', color=BLACK)
+xaxis_label = label.Label(
+    x=graph_x0 + graph_width // 2 - 12, y=display.height - 10, font=FONT, text="days", color=BLACK
+)
 graph.append(xaxis_label)
 
 # special runtime # in corner
@@ -167,9 +170,19 @@ display_group.append(runtime_text)
 # Draw graph ticks
 yticks_group = displayio.Group()
 for i in range(yticks + 1):
-    yticks_group.append(Line(graph_x0 - tick_halfwidth, graph_y0 - i * py_tick, graph_x0 + tick_halfwidth, graph_y0 - i * py_tick, color=BLACK))
+    yticks_group.append(
+        Line(
+            graph_x0 - tick_halfwidth,
+            graph_y0 - i * py_tick,
+            graph_x0 + tick_halfwidth,
+            graph_y0 - i * py_tick,
+            color=BLACK,
+        )
+    )
     if (i % 2) == 1:
-        yticks_group.append(label.Label(x=graph_x0 - 18, y=graph_y0 - i * py_tick, font=FONT, text=str(i * 10), color=BLACK))
+        yticks_group.append(
+            label.Label(x=graph_x0 - 18, y=graph_y0 - i * py_tick, font=FONT, text=str(i * 10), color=BLACK)
+        )
 graph.append(yticks_group)
 
 # Set up graph object (initially empty)
@@ -196,6 +209,7 @@ graph.append(current_rh_text)
 display_group.append(graph)
 display.root_group = display_group
 
+
 ## functions to update graph and current_rh_text with actual data
 def update_rh_data():
     global rh_data
@@ -207,11 +221,14 @@ def update_rh_data():
     print(f"humidity = {current_rh}%, saving to slot {rh_data_index} in data buffer")
     rh_data[rh_data_index] = int(current_rh + 0.5)  # round
 
+
 def mean(list):
     return sum(list) / len(list)
 
+
 def remove_zeros(lst):
     return [x for x in lst if x != 0]
+
 
 def min_nonzero(lst):
     """Return smallest non-zero element in list, or 0 if empty list"""
@@ -221,6 +238,7 @@ def min_nonzero(lst):
     else:
         return min(lst)
 
+
 def mean_nonzero(lst):
     """Return average non-zero element in list, or 0 if empty list"""
     lst = remove_zeros(lst)
@@ -229,11 +247,13 @@ def mean_nonzero(lst):
     else:
         return mean(lst)
 
+
 def scale_and_clip(rh):
     """Convert RH data into pixel Y position, with some clipping safety checks."""
     d = min(rh_max, max(rh, 0))
     dy = int(d * py_per_rh)
     return dy
+
 
 def wrapped_slice(lst, i0, width):
     """Return a width slice from lst as a circular buffer, wrapping around if needed"""
@@ -241,12 +261,13 @@ def wrapped_slice(lst, i0, width):
     if width > len(lst):
         return lst
     if i0 + width <= len(lst):
-        return lst[i0:i0+width]  # normal slice
+        return lst[i0 : i0 + width]  # normal slice
     else:
         a = lst[i0:]
         wrap = i0 + width - len(lst)
         b = lst[:wrap]
         return a + b
+
 
 def update_graph():
     """Update graph (overwriting data_group object) with data pulled from global rh_data buffer."""
@@ -261,37 +282,15 @@ def update_graph():
         x = graph_x0 + data_x0 + (b + 1) * px_tick
         if davg_y != 0:
             # if bin has data
-            data_group.append(Line(x0 = x, y0 = graph_y0 - dmin_y, x1 = x, y1 = graph_y0  - dmax_y, color=BLACK))
-            data_group.append(Circle(x, graph_y0 - davg_y, r = marker_size, fill=BLACK, outline=None))
+            data_group.append(Line(x0=x, y0=graph_y0 - dmin_y, x1=x, y1=graph_y0 - dmax_y, color=BLACK))
+            data_group.append(Circle(x, graph_y0 - davg_y, r=marker_size, fill=BLACK, outline=None))
     # add most recent data
     drecent_y = scale_and_clip(rh_data[rh_data_index])
-    data_group.append(Circle(x, graph_y0 - drecent_y, r = highlight_marker_size, fill=RED, outline=None))
+    data_group.append(Circle(x, graph_y0 - drecent_y, r=highlight_marker_size, fill=RED, outline=None))
     # replace past data_group object
     graph.pop(data_group_index)
     graph.insert(data_group_index, data_group)
     # update current RH values
-    current_rh_text[0].text = f"{rh_data[rh_data_index]}"
-    rh_y = graph_y0 - int(rh_data[rh_data_index] * py_per_rh)
-    rh_y = min(100, max(20, rh_y))
-    current_rh_text.y = rh_y
-
-# update graph, pulling data from global rh_data buffer
-def update_graph_old():
-    for i in range(len(rh_data)):
-        # start by pulling data from oldest location (i.e. one beyond current data index in circular buffer)
-        i_rel = (i + rh_data_index + 1) % len(rh_data)
-        val = min(rh_max, max(rh_data[i_rel], 0))
-        c = BLACK
-        r = marker_size
-        if i == len(rh_data) - 1:
-            c = RED
-            r = marker_size * 2
-        dy = int(val * py_per_rh)
-        if val == 0:
-            dy = -100  # offset zero (invalid?) data off the screen
-        data_group[i].y0 = graph_y0 - dy
-        data_group[i].r = r
-        data_group[i].fill = c
     current_rh_text[0].text = f"{rh_data[rh_data_index]}"
     rh_y = graph_y0 - int(rh_data[rh_data_index] * py_per_rh)
     rh_y = min(100, max(20, rh_y))
@@ -306,7 +305,7 @@ while True:
         for i in range(40):
             update_rh_data()
     update_graph()
-    run_cycles += 1 
+    run_cycles += 1
     # actually update E Ink screen
     display.refresh()
     ## deep sleep until next update period
